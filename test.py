c28505bf97e5efd2d1249bdb08041d3a6c37423a
Springhser <chenjinbincjb@163.com>
2021-08-20 13:35:28 +0800

    refactor the code

--- test.py
+++ test.py
@@ -4,31 +4,70 @@ version:
 Author: springhser
 Date: 2021-08-13 11:46:37
 LastEditors: springhser
-LastEditTime: 2021-08-13 14:57:47
+LastEditTime: 2021-08-18 15:50:36
 '''
 from scipy.spatial.transform import Rotation as R
 import numpy as np
 
-r = R.from_euler("xyz", [0,-45,0], degrees=True)
-print(r.as_matrix())
-tm = r.as_matrix()
+
+def reshapeMatrix(mat):
+    E = np.eye(4,4)
+    # print(E)
+    for i in range(0,3):
+        for j in range(0,3):
+            t = mat[0][i][j]
+            E[i][j] = t
+
+    # print(E)
+    return E
+
+
+def getTranslateMatrix(x, y, z):
+    E = np.eye(4,4)
+    E[0][3] = x
+    E[1][3] = y
+    E[2][3] = z
+    return E
+
+# r = R.from_euler("xyz", [0,-45,0], degrees=True)
+# print(r.as_matrix())
+# tm = r.as_matrix()
 # print(type(tm))
 
-I = 0
-J = 45
-K = 0
 
-Rx = R.from_euler("x",[I], degrees=True).as_matrix()
-Ry = R.from_euler("y",[J], degrees=True).as_matrix()
-Rz = R.from_euler("z",[K], degrees=True).as_matrix()
 
-print(Rx)
-print(Ry)
-print(Rz)
+if __name__ == "__main__":
+    I = 0
+    J = 45
+    K = 0
+    X = 50
+    Y = -50
+    Z = 0
+    Rx = R.from_euler("x",[I], degrees=True).as_matrix()
+    Ry = R.from_euler("y",[J], degrees=True).as_matrix()
+    Rz = R.from_euler("z",[K], degrees=True).as_matrix()
 
-p = np.array([1,0,0])
-p1 = tm.dot(p)
+    # print(Rx)
+    # print(Ry)
+    # print(Rz)
+    # print(Rx.shape)
 
-print(p1)
+    Ry4 = reshapeMatrix(Ry)
+    Rx4 = reshapeMatrix(Rx)
+    Rz4 = reshapeMatrix(Rz)
 
-# translation_matrix = np.array()
+
+    Tmat = getTranslateMatrix(X, Y, Z)
+
+    print(Tmat)
+
+    Mat = Tmat@Rz4@Ry4@Rx4
+
+    print(Mat)
+
+    
+    p = np.array([1,0,0,1])
+
+    Res = np.matmul(Mat, p)
+
+    print(Res)
